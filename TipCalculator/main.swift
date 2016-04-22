7949c248122152a779f1013215e6e1eb70dd94f7
Joseph Li <josephyli@gmail.com>
2016-04-22 13:26:17 -1000

    Initial Commit

---
+++ TipCalculator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TipCalculator
+//
+//  Created by Joseph Li on 4/22/16.
+//  Copyright © 2016 Broseph. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
