b666372b6cf3706f9178eea862c575551cd2e8e3
Joseph Li <josephyli@gmail.com>
2016-04-28 11:43:52 -1000

    First commit

--- DaysUntil/main.swift
+++ DaysUntil/main.swift
@@ -8,5 +8,21 @@
 
 import Foundation
 
-print("Hello, World!")
+let now = NSDate()
 
+print("Let's find how many days until a certain date!")
+
+print("Please enter a date (MM-dd-yyyy): ", terminator: "")
+let read = readLine()
+
+var dateFormatter = NSDateFormatter()
+    dateFormatter.dateFormat = "MM-dd-yy"
+var userDate = dateFormatter.dateFromString(read!)
+dateFormatter.locale = NSLocale(localeIdentifier: "en_US")
+
+dateFormatter.dateStyle = .FullStyle
+let userCalendar = NSCalendar.currentCalendar()
+let calendarUnit: NSCalendarUnit = [.Day]
+let dayDifference = userCalendar.components(calendarUnit, fromDate: now, toDate: userDate!, options: [])
+
+print("There are \(abs(dayDifference.day)) days until \(userDate!)")
