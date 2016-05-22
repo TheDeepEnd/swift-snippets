8a22b2a4075b5aaef52bbe7da789e916f8bf4d83
Joseph Li <josephyli@users.noreply.github.com>
2016-05-21 17:13:17 -1000

    Cleaned up code. Fixed bugs
    Code is cleaner. Date comparison now based on the start of the day for fair date comparisons.

--- DaysUntil/main.swift
+++ DaysUntil/main.swift
@@ -8,21 +8,48 @@
 
 import Foundation
 
-let now = NSDate()
+print("This app calculates how many days until a certain date.")
 
-print("Let's find how many days until a certain date!")
+// set up the calendar object
+let calendar = NSCalendar.currentCalendar()
 
-print("Please enter a date (MM-dd-yyyy): ", terminator: "")
-let read = readLine()
+// set up date object for today's date
+let todaysDate = calendar.startOfDayForDate(NSDate())
 
+// set up formatter of valid input string as dates
 var dateFormatter = NSDateFormatter()
-    dateFormatter.dateFormat = "MM-dd-yy"
-var userDate = dateFormatter.dateFromString(read!)
+dateFormatter.dateFormat = "M-d-y"
 dateFormatter.locale = NSLocale(localeIdentifier: "en_US")
 
-dateFormatter.dateStyle = .FullStyle
-let userCalendar = NSCalendar.currentCalendar()
-let calendarUnit: NSCalendarUnit = [.Day]
-let dayDifference = userCalendar.components(calendarUnit, fromDate: now, toDate: userDate!, options: [])
+var stringFromUser: String
 
-print("There are \(abs(dayDifference.day)) days until \(userDate!)")
+repeat {
+    print("Please enter a date (MM-dd-yyyy): ", terminator: "")
+    // read in user date
+    stringFromUser = readLine()!
+    
+    // catch bad input by using NSDateFormatter
+    if dateFormatter.dateFromString(stringFromUser) == nil {
+        print("Sorry, the date you entered is invalid.")
+        stringFromUser = ""
+    }
+} while (stringFromUser.isEmpty)
+
+
+// convert the string into a date, from the beginning of the day
+var userDate = calendar.startOfDayForDate(dateFormatter.dateFromString(stringFromUser)!)
+
+// format the date into a more readable format ("May 23, 2016" instead of "5-23-2016")
+dateFormatter.dateStyle = NSDateFormatterStyle.LongStyle
+
+
+let nowString = dateFormatter.stringFromDate(todaysDate)
+let laterString = dateFormatter.stringFromDate(userDate)
+
+let calendarUnit: NSCalendarUnit = .Day
+let dayDifference = abs(calendar.components(calendarUnit, fromDate: todaysDate, toDate: userDate, options: []).day)
+
+print("\nYou entered \(laterString)")
+print("Today is \(nowString)\n")
+
+print("There are \(dayDifference) days from today until \(laterString)")
